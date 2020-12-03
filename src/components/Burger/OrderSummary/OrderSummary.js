a7228ceb6182b7a49d4c382464d970f459533e98
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-12-03 00:32:47 -0700

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients).map((igKey) => {
+class OrderSummary extends Component {
+    componentDidUpdate(){
+        console.log('[OrderSummary] DidUpdated')
+    }
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients).map((igKey) => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}> {igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            )
+        });
         return (
-            <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}> {igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        )
-    });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicius burger with the following ingrgedients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicius burger with the following ingrgedients:</p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-orderSummary.propTypes = {
+OrderSummary.propTypes = {
     ingredients: PropTypes.object.isRequired,
     price: PropTypes.number.isRequired,
     purchseCanceled: PropTypes.func.isRequired,
     purchaseContinued: PropTypes.func.isRequired
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+    componentDidUpdate() {
+        console.log('[Modal DidUpdate')
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.prsops.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
-modal.propTypes = {
+Modal.propTypes = {
     show: PropTypes.bool.isRequired,
     modalClosed: PropTypes.func.isRequired
 }
 
-export default modal;
+export default Modal;
