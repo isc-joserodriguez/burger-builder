4e16684725c931dc102a75bcdf4a6cfa57b3c1fa
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2020-12-03 00:52:52 -0700

    Changing the Folder Structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
@@ -15,7 +15,7 @@ class Modal extends Component {
     render() {
         return (
             <Aux>
-                <Backdrop show={this.prsops.show} clicked={this.props.modalClosed} />
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
                 <div
                     className={classes.Modal}
                     style={{
