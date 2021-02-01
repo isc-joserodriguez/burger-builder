b62b93280e2922e5279d4c4047dae4bacfd856fb
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-31 20:01:24 -0700

    Redirect to Improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -16,16 +16,24 @@ class Checkout extends Component {
     }
 
     render() {
+        let summary = <Redirect to="/" />;
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        CheckoutCancelled={this.CheckoutCancelled}
+                        CheckoutContinued={this.CheckoutContinued} />
+                    <Route
+                        path={this.props.match.path + '/contact-data'}
+                        component={ContactData} />
+                </div>
+            );
+        };
 
         return (
             <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    CheckoutCancelled={this.CheckoutCancelled}
-                    CheckoutContinued={this.CheckoutContinued} />
-                <Route
-                    path={this.props.match.path + '/contact-data'}
-                    component={ContactData} />
+                {summary}
             </div>
         )
     }
