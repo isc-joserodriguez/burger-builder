df508702457f2a5f4cf437e71244765036105d7f
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-01 13:14:55 -0700

    Refactoring Reducers II

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -12,7 +12,6 @@ class Orders extends Component {
         this.props.onFetchOrders();
     }
     render() {
-        console.log(this.props);
         let orders = <Spinner />;
         if (!this.props.loading) {
             orders = this.props.orders.map(order => (
