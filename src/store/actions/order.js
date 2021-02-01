792e00ec8dd58366afa1a7624566ce1f62194d24
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-31 19:03:31 -0700

    Working on Order Actions

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -13,16 +13,23 @@ export const purchaseBurgerFail = (error) => {
     return {
         type: actionTypes.PURCHSE_BURGER_FAIL,
         error: error
-    }
-}
+    };
+};
 
-export const purchaseBurgerStart = (orderData) => {
+export const purchaseBurgerStart = () => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
+    };
+};
+
+export const purchaseBurger = (orderData) => {
     return dispatch => {
+        dispatch(purchaseBurgerStart());
         axios.post('/orders.json', orderData).then(response => {
             dispatch(purchaseBurgerSuccess(response.data, orderData));
         }).catch(error => {
             dispatch(purchaseBurgerFail(error));
 
         });
-    }
-}
+    };
+};
