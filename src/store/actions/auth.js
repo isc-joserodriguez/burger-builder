c6a08ecf83305abb1fe6126499a5a74b24a7d6d5
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-14 13:26:11 -0700

    Persistent Auth State with localStorage

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -9,6 +9,7 @@ export const authStart = () => {
 };
 
 export const authSuccess = (token, userId) => {
+
     return {
         type: actionTypes.AUTH_SUCCESS,
         idToken: token,
@@ -24,6 +25,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -49,6 +53,10 @@ export const auth = (email, password, isSignup) => {
         if (!isSignup) url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
         axios.post(`${url}?key=AIzaSyBhzgiFUlkHgotZYUR97-kA7xn1ayzJXoE`, authData)
             .then(response => {
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -62,5 +70,24 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationTime = new Date(localStorage.getItem('expirationDate'));
+            if (expirationTime > new Date()) {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationTime.getSeconds() - new Date().getSeconds()));
+            } else {
+                dispatch(logout());
+            }
+
+        }
     };
 };
