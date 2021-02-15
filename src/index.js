80ecbfac4ef66b5efad7ed7489704d91700deca7
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-02-15 16:35:44 -0700

    Building the Project

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const store = createStore(rootReducer, composeEnhancers(
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/burger-builer/live/">
       <App />
     </BrowserRouter>
   </Provider>
